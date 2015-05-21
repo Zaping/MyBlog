5e2cb3639cedd67970407153a38f3ab8ae86e0ce
Zaping <cemcavdar7b@hotmail.com>
2015-05-21 19:03:11 +0300

    Pagination search and sort

--- MVCBlog/MVCBlog/Controllers/HomeController.cs
+++ MVCBlog/MVCBlog/Controllers/HomeController.cs
@@ -1,14 +1,55 @@
 ﻿using MVCBlog.Models;
 using System.Web.Mvc;
 using System.Linq;
+using PagedList;
+using System.Collections.Generic;
 namespace MVCBlog.Controllers
 {
     public class HomeController : Controller
     {
         private BlogDBContext db = new BlogDBContext();
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder,string currentFilter,string searchString,int? page)
         {
-            return View(db.Blogs.ToList());
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+            var students = from s in db.Blogs
+                           select s;
+            if (!string.IsNullOrEmpty(searchString))
+            {
+                students = students.Where(s => s.Title.Contains(searchString)
+                                       || s.Content.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    students = students.OrderByDescending(s => s.Title);
+                    break;
+                case "Date":
+                    students = students.OrderBy(s => s.EntryDate);
+                    break;
+                case "date_desc":
+                    students = students.OrderByDescending(s => s.EntryDate);
+                    break;
+                default:
+                    students = students.OrderBy(s => s.Content);
+                    break;
+            }
+            
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            return View(students.ToPagedList(pageNumber, pageSize));
+
         }
 
         public ActionResult About()
